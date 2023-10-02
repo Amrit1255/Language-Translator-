63f2bf001ea056d295bd02b1b15404eb644442e2
Amrit <tiwariamrit303@gmail.com>
2023-10-02 17:29:30 +0530

    Initial Commit

---
+++ js/countries.js
@@ -0,0 +1,17 @@
+const countries = {
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "ne-NP": "Nepali",
+    "pa-IN": "Panjabi",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+    "as-IN": "Assamese",
+    "bho-IN": "Bhojpuri",
+    "mai-IN": "Maithili",
+    "or-IN": "Odia",
+    "sa-IN": "Sanskrit",
+    "ur-IN": "Urdu",
+    "ma-IN": "Manipuri"
+}
